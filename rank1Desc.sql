26a9243b09f4cfe8ea0a917438144fab9baedb1a
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-03-03 11:04:38 +0530

    Create rank1Desc.sql

---
+++ rank1Desc.sql
@@ -0,0 +1,13 @@
+SELECT 
+	-- Select the league name and average goals scored
+	l.name AS league,
+    avg(m.home_goal + m.away_goal) AS avg_goals,
+    -- Rank leagues in descending order by average goals
+    rank() over(order by avg(m.home_goal + m.away_goal) desc) AS league_rank
+FROM league AS l
+LEFT JOIN match AS m 
+ON l.id = m.country_id
+WHERE m.season = '2011/2012'
+GROUP BY l.name
+-- Order the query by the rank you created
+order by league_rank;
