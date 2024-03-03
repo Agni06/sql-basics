e84f1e30b454456944aa1a28314096aa6075aa60
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-03-03 11:18:38 +0530

    Create partitioned1.sql

---
+++ partitioned1.sql
@@ -0,0 +1,17 @@
+SELECT 
+	date,
+	season,
+	home_goal,
+	away_goal,
+	CASE WHEN hometeam_id = 8673 THEN 'home' 
+         ELSE 'away' END AS warsaw_location,
+	-- Calculate average goals partitioned by season and month
+    avg(home_goal) over(partition by season, 
+         	EXTRACT(month FROM date)) AS season_mo_home,
+    avg(away_goal) over(partition by season, 
+            EXTRACT(month FROM date)) AS season_mo_away
+FROM match
+WHERE 
+	hometeam_id = 8673
+    OR awayteam_id = 8673
+ORDER BY (home_goal + away_goal) DESC;
