654d5bf54bb864e62ed5fd8611d32a216454004d
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-03-14 23:43:10 +0530

    Create values.sql

---
+++ values.sql
@@ -0,0 +1,6 @@
+--What's wrong? Correct the university_id so that it actually reflects where Albert Einstein wrote 
+--his dissertation and became a professor – at the University of Zurich (UZH)!
+
+-- Try to insert a new professor
+INSERT INTO professors (firstname, lastname, university_id)
+VALUES ('Albert', 'Einstein', 'UZH');
